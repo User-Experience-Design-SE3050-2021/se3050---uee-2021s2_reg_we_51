c55de69663d09ee62b0316ac479ccfa219ca8389
GayashaBudddhi <it19061580@my.sliit.lk>
2021-10-02 17:56:25 +0530

    pay bills updated

---
+++ app/src/main/java/com/example/uee_ndb_neos_bankingapp/BillPayment.java
@@ -0,0 +1,14 @@
+package com.example.uee_ndb_neos_bankingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BillPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bill_payment);
+    }
+}
---
+++ app/src/main/java/com/example/uee_ndb_neos_bankingapp/FundTransactionForm.java
@@ -0,0 +1,6 @@
+package com.example.uee_ndb_neos_bankingapp;
+
+import android.app.Activity;
+
+public class FundTransactionForm extends Activity {
+}

936a3162306d0433fd92feb2044a1af30191d07f
TAN0013\Tandin Wangchen <tandin.wangchen@trabeya.com>
2021-10-07 11:50:25 +0530

    alert added

--- app/src/main/java/com/example/uee_ndb_neos_bankingapp/ui/fund/FundTransactionForm.java
+++ app/src/main/java/com/example/uee_ndb_neos_bankingapp/ui/fund/FundTransactionForm.java
@@ -1,7 +1,9 @@
 package com.example.uee_ndb_neos_bankingapp.ui.fund;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -9,16 +11,62 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.uee_ndb_neos_bankingapp.MainActivity;
 import com.example.uee_ndb_neos_bankingapp.R;
 
 public class FundTransactionForm extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Button fundbuton;
+    AlertDialog.Builder builder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fund_transaction_form);
+
+        fundbuton = (Button) findViewById(R.id.button);
+        builder = new AlertDialog.Builder(this);
+        fundbuton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Uncomment the below code to Set the message and title from the strings.xml file
+               // builder.setMessage("messageee") .setTitle("R.string.dialog_title");
+
+                //Setting message manually and performing action on button click
+                builder.setMessage("Do you want to Pay ?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //finish();
+
+                                Intent intent = new Intent(getApplicationContext(),  MainActivity.class);
+
+                                Toast.makeText(getApplicationContext(),"Paid",
+                                        Toast.LENGTH_SHORT).show();
+                                startActivity(intent);
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //  Action for 'NO' Button
+                                dialog.cancel();
+                                Toast.makeText(getApplicationContext(),"Canceled",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                //Creating dialog box
+                AlertDialog alert = builder.create();
+                //Setting the title manually
+                alert.setTitle("AlertDialogExample");
+                alert.show();
+            }
+        });
+
+
 
         Spinner spinner = findViewById(R.id.spinner1);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.payee_list, android.R.layout.simple_spinner_item);
@@ -34,8 +82,9 @@ public class FundTransactionForm extends AppCompatActivity implements AdapterVie
     }
     public void sendToHome(View view) {
         Intent intent = new Intent(this,  MainActivity.class);
-        Button button = (Button) findViewById(R.id.button);
-        startActivity(intent);
+
+
+        //startActivity(intent);
     }
 
     @Override

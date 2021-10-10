2fea55efc79c3457b778cff4bef8a8217567a5e1
Buddhi Amarasinghe <68139319+GayashaBudddhi@users.noreply.github.com>
2021-10-10 09:24:55 +0530

    add biller backend issue fixed

--- app/src/main/java/com/example/uee_ndb_neos_bankingapp/ui/bills/AddBiller.java
+++ app/src/main/java/com/example/uee_ndb_neos_bankingapp/ui/bills/AddBiller.java
@@ -2,16 +2,16 @@ package com.example.uee_ndb_neos_bankingapp.ui.bills;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-        import android.os.Bundle;
+import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-        import android.widget.AdapterView;
-        import android.widget.ArrayAdapter;
-        import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-        import android.widget.Spinner;
-        import android.widget.Toast;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
